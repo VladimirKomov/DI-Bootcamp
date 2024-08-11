c55817ad94f0a61140476130b349603ccab686e5
VladimirKomov <v.e.komov@gmail.com>
2024-08-11 18:35:14 +0300

    changed metods of the class farm

--- Week2/Day1/DailyChallenge/challenge.py
+++ Week2/Day1/DailyChallenge/challenge.py
@@ -5,38 +5,22 @@ class Farm:
         self.name = name
 
     def add_animal(self, type: str, count=1):
-        if type in self.dict_animals:
-            self.dict_animals[type] = (self.dict_animals.get(type) + count)
-        else:
-            self.dict_animals[type] = count
+        self.dict_animals[type] = self.dict_animals.setdefault(type, 0) + count
 
     def get_info(self):
         info = f"{self.name}'s farm\n\n"
-        for animal, count in self.dict_animals.items():
-            info += f"{animal} : {count}\n"
-        info += "\n    E-I-E-I-0!\n"
+        list_info = [f"{animal} : {count}\n" for animal, count in self.dict_animals.items()]
+        info += "".join(list_info) + "\n    E-I-E-I-0!\n"
         return info
 
     def get_animal_types(self) -> list:
-        types = list(self.dict_animals.keys())
-        list.sort(types)
-        return types
+        types = self.dict_animals.keys()
+        return sorted(types)
 
     def get_short_info(self):
         info = f"{self.name}’s farm has "
-        list_info = []
-
-        for alimal, count in self.dict_animals.items():
-            if count > 1:
-                list_info.append(f"{alimal}s")
-            else:
-                list_info.append(f"{alimal}")
-        if len(list_info) > 1:
-            info += ", ".join(list_info[:-1]) + f" and {list_info[-1]}."
-        # for one elemrnt
-        else:
-            info += f"{list_info[0]}."
-
+        list_info = [f"{animal}s" if count > 1 else f"{animal}" for animal, count in self.dict_animals.items()]
+        info += ", ".join(list_info[:-1]) + f" and {list_info[-1]}." if len(list_info) > 1 else f"{list_info[0]}."
         return info
 
 
@@ -48,4 +32,4 @@ macdonald.add_animal('goat', 12)
 
 print(macdonald.get_info())
 print(macdonald.get_animal_types())
-print(macdonald.get_short_info())
+print(macdonald.get_short_info)
