8c2916261647533d43e0efd209812141d49fbef9
VladimirKomov <v.e.komov@gmail.com>
2024-08-15 18:22:13 +0300

    added files game r-c-s

--- Week2/Day5/ExerciseXP/game/game.py
+++ Week2/Day5/ExerciseXP/game/game.py
@@ -1,17 +1,21 @@
-class Game:
-
-    def get_user_item(self):
-        input('Select (r)ock, (p)aper, or (s)cissor: ')
+import random
+from typing import Union
 
 
 # classes are more convenient to compare
-class Rock:
+class GameItem:
+    pass
+
+
+class Rock(GameItem):
     # equally
     def __eq__(self, other):
         return isinstance(other, Rock)
+
     # less
     def __lt__(self, other):
         return isinstance(other, Paper)
+
     # more
     def __gt__(self, other):
         return isinstance(other, Scissor)
@@ -19,13 +23,16 @@ class Rock:
     def __str__(self):
         return "Rock"
 
-class Paper:
+
+class Paper(GameItem):
     # equally
     def __eq__(self, other):
         return isinstance(other, Paper)
+
     # less
     def __lt__(self, other):
         return isinstance(other, Scissor)
+
     # more
     def __gt__(self, other):
         return isinstance(other, Rock)
@@ -33,16 +40,70 @@ class Paper:
     def __str__(self):
         return "Paper"
 
-class Scissor:
+
+class Scissor(GameItem):
     # equally
     def __eq__(self, other):
         return isinstance(other, Scissor)
+
     # less
     def __lt__(self, other):
         return isinstance(other, Rock)
+
     # more
     def __gt__(self, other):
         return isinstance(other, Paper)
 
     def __str__(self):
         return "Scissor"
+
+
+class Game:
+
+    def __init__(self):
+        self.__results_dist = {"won": 0, "lost": 0, "drew": 0}
+
+    @property
+    def results_dist(self):
+        return self.__results_dist
+
+
+    def play(self):
+        user_item = self.__get_user_item()
+        computer_item = self.__get_computer_item()
+        result = self.__get_game_result(user_item, computer_item)
+        print(f'You selected {user_item}. The computer selected {computer_item}. {result.upper()}')
+        self.results_dist[result] += 1
+    def __get_user_item(self) -> GameItem:
+        user_item = None
+        while not user_item:
+            user_input = input('Select (r)ock, (p)aper, or (s)cissor: ')
+            user_item = self.__make_item(user_input.lower())
+            if not user_item:
+                print("Invalid selection. Please choose 'r' for Rock, 'p' for Paper, or 's' for Scissor.")
+        return user_item
+
+    def __get_computer_item(self) -> GameItem:
+        items_list = [Rock(), Paper(), Scissor()]
+        return random.choice(items_list)
+
+    def __make_item(self, chose: str) -> Union[GameItem, None]:
+        if chose == 'r':
+            return Rock()
+        elif chose == 'p':
+            return Paper()
+        elif chose == 's':
+            return Scissor()
+        else:
+            return None
+
+    # get_game_result(self, user_item, computer_item) – Determine the result of the game.
+    def __get_game_result(self, user_item: GameItem,
+                          computer_item: GameItem) -> str:
+        if user_item > computer_item:
+            return 'won'
+        elif user_item < computer_item:
+            return 'lost'
+        else:
+            return 'drew'
+
---
+++ Week2/Day5/ExerciseXP/game/rock-paper-scissors.py
@@ -0,0 +1,41 @@
+from Week2.Day5.ExerciseXP.game.game import Game
+
+
+def get_user_menu_choice():
+    user_input = input('(g) Play a game \n'
+                       '(q) Show scores and Quit \n '
+                       ':')
+    return user_input
+
+def print_results(results: dict):
+    print(f"Game results:\n"
+          f"    You won  {results["won"]} times\n"
+          f"    You loss {results["lost"]} times\n"
+          f"    You drew {results["drew"]} times\n"
+          "Thank you for your playing!")
+
+# Displaying the menu repeatedly, until the user types in the value to exit the program:
+# (Make use of the get_user_menu_choice function)
+game = None
+def main():
+    global game
+    user_input = get_user_menu_choice().lower()
+    if user_input not in ('g', 'q'):
+        print("Invalid input. Please choose 'g' for game, 'q' for result and quit")
+        main()
+    # When the user chooses to play a game.
+    # Remember the results of every game that is played.
+    if game is None:
+        game = Game()
+
+    if user_input == 'g':
+        game.play()
+        main()
+
+    if user_input == 'q':
+        print_results(game.results_dist)
+
+main()
+
+
+
