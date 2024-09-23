1562ff3b3f55b6539b5604ce26085779c7b96335
VladimirKomov <v.e.komov@gmail.com>
2024-09-23 16:51:54 +0300

    Set up initial project files for carousel app

---
+++ Week8/Day1/DailyChallenge/carousel/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import CarouselComponent from "./CarouselComponent.jsx";
+
+function App() {
+  return (
+      <div>
+          <main>
+              <CarouselComponent/>
+          </main>
+      </div>
+  )
+}
+
+export default App
